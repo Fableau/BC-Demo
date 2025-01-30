5c88ae10438f29ebc40e7fb2dafcd08e176aa9c4
Vincent Oldenburg <oldenburgva@gmail.com>
2025-01-30 23:54:28 +0100

    replace file with original

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,18 @@
-import Header from "@/components/Header/Header";
-import Wrapper from "@/components/Wrapper/Wrapper";
+import "@/styles/globals.scss";
+import "@/styles/fonts.scss";
 
-export const metadata = {
-  title: "Motorhuis Laadpunt Samenstellen",
-  description: "Beschrijving",
-};
+// Load FontAwesome CSS before icons are rendered, removing visual bug (e.g. movement / scaling after first render).
+// AutoAddCSS should be off, since we don't need the CSS twice.
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; 
 
-const FormLayout = ({ children }) => (
-  <Wrapper>
-    <Header />
-    { children }
-  </Wrapper>
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      {children}
+    </body>
+  </html>
 )
 
-export default FormLayout;
+export default RootLayout;
